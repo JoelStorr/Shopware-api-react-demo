a7c14fdcca46dfaf028f7a9bbd1c421a375581ad
Joel Storr <joel.storr@ewe.net>
2023-03-30 21:30:23 +0200

    Queried for Categories from Store API

---
+++ src/components/nav/Navigation.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
+
+
+export default function Navigation(){
+
+      const shopwareDomain = "http://localhost/store-api/";
+      const [categories, setCategories] = useState([]);
+
+      useEffect(() => {
+        apiVal();
+      }, []);
+
+      const apiVal = () => {
+        axios({
+          method: "post",
+          url: `${shopwareDomain}category`,
+          headers: { "sw-access-key": "SWSCWDHDQLQ4UM9YZZZIEUXLBQ" },
+        }).then((res) => {
+          console.log(res.data.elements);
+          setCategories(res.data.elements);
+        });
+      };
+
+    return(
+        <ul>
+            {categories.map((cat)=>{
+                return <li key={cat.id}>{cat.name}</li>
+            })}
+        </ul>
+    )
+
+
+}
