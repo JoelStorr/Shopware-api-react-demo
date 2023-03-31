1c7f3923e933e5c7639306ef56dc963430f1816e
Joel Storr <joel.storr@ewe.net>
2023-03-31 12:51:01 +0200

    Added API Call, Started destructuring of data

--- src/components/Header/nav/Navigation.jsx
+++ src/components/Header/nav/Navigation.jsx
@@ -7,10 +7,15 @@ import axios from "axios";
 export default function Navigation(){
 
       const shopwareDomain = "http://localhost/store-api/";
+
+      const [dataLoaded, setDataLoaded] = useState(false);
       const [categories, setCategories] = useState([]);
 
       useEffect(() => {
-        apiVal();
+        if(!dataLoaded){
+          apiVal();
+          setDataLoaded(true)
+        }
       }, []);
 
       //NOTE: Change API Key
@@ -21,15 +26,52 @@ export default function Navigation(){
           headers: { "sw-access-key": "SWSCWDHDQLQ4UM9YZZZIEUXLBQ" },
         }).then((res) => {
           console.log(res.data.elements);
-          setCategories(res.data.elements);
+          orderNav(res.data.elements);
         });
       };
 
+      //NOTE: Only allows for one Child Categorie right now
+      //TODO: Make sure that there are no duplicates in Categories Array when data is called again.
+
+      function orderNav(queryResElemetns){
+        
+      
+
+        queryResElemetns.forEach(element => {
+          console.info(element);
+
+          if(element.parentId == null){
+            setCategories(() => {
+              categories.push({ parent: element, children: [] });
+            });
+          }
+
+
+
+      /*     if(element.parentId === null){
+            setCategories(()=>{categories.push({ parent: element, children: [] });})
+          }else{
+            if(categories.length < 1 ){
+              console.error('Something whent wrong when fetching Parent Data');
+              return;
+            }
+            let parentCategorei = categories.find(ele => ele.id === element.parentId);
+            if(parentCategorei){
+              parentCategorei.children.push(element);
+            }
+          } */
+
+      
+        });
+
+
+        console.log(categories);
+
+      }
+
     return(
         <ul>
-            {categories.map((cat)=>{
-                return <li key={cat.id}>{cat.name}</li>
-            })}
+           
         </ul>
     )
 
