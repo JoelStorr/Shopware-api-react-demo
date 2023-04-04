816a143de2036e349d5d1ea07bdadc80dab970b8
Joel Storr <joel.storr@ewe.net>
2023-04-04 21:20:24 +0200

    Added Baisc Camera Scroll Animation

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,84 +9,78 @@ import './Main.scss';
 import { useScroll } from '@react-three/drei';
 
 export default function Main(){
-
-
   const scroll = useScroll();
   const tl = useRef();
-  const camera = useThree(state => state.camera);
+  const camera = useThree((state) => state.camera);
 
+  useFrame((state, delta) => {
+    const angle = state.clock.elapsedTime;
+    //state.camera.position.x = Math.sin(angle) * 8;
+    //state.camera.position.z = Math.cos(angle) * 8;
+    state.camera.lookAt(0, 1, 0);
 
+    tl.current.seek(scroll.offset * tl.current.duration());
+    console.log(scroll.offset * tl.current.duration());
+   /* console.log(state.camera.position); */
+    //console.log(scroll.offset)
+  });
 
+  useLayoutEffect(() => {
+    tl.current = gsap.timeline({
+      defaults: { duration: 6, ease: "linear" },
+    });
 
-      useFrame((state, delta) => {
-        const angle = state.clock.elapsedTime;
-        //state.camera.position.x = Math.sin(angle) * 8;
-        //state.camera.position.z = Math.cos(angle) * 8;
-        state.camera.lookAt(0, 0, 0);
-
-        tl.current.seek(scroll.offset * tl.current.duration());
-
-        console.log(scroll.offset)
-      });
-
-      useLayoutEffect(()=>{
-        tl.current = gsap.timeline({
-          defaults: {duration: 2, ease: "power1.inOut"},
-        })
-
-        tl.current
-          .to(camera.position, {x: -1.5}, 2)
-          .to(camera.position, {x: -1.4}, 6)
-          .to(camera.position, {x: -1.3}, 11)
-          .to(camera.position, {x: -1.2}, 13)
-          .to(camera.position, {x: -1.1}, 16)
-          .to(camera.position, {x: -1.0}, 20)
-
-        console.log(camera.position)
-      },[])
-
+    tl.current
+      /* .from(camera.position, { x: -1.5, y: 2.5, z: -2.5 }, 0)
+      */
+      .to(camera.position, { x: 2.63, y:1 , z: 0.01 }, 2)
+      .to(camera.position, { x: 2.63, y:1 , z: 0.01 }, 4)
+      .to(camera.position, { x: 2.63, y:1 , z: 0.01 }, 6)
+  
       
 
+    //console.log(camera.position);
+  }, []);
 
+  
 
-    return (
-      <>
-        {/* Center Block */}
-        <mesh scale={[3, 1, 3]}>
-          <boxGeometry />
-          <meshNormalMaterial />
-        </mesh>
+  return (
+    <>
+      {/* Center Block */}
+      <mesh scale={[3, 1, 3]}>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
 
-        {/* Fridge */}
-        <mesh scale={[1, 2, 1]} position={[4, 0.5, -1.5]}>
-          <boxGeometry />
-          <meshNormalMaterial />
-        </mesh>
+      {/* Fridge */}
+      <mesh scale={[1, 2, 1]} position={[4, 0.5, -1.5]}>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
 
-        {/* Countertop left */}
-        <mesh scale={[1, 1, 4]} position={[4, 0, 1]}>
-          <boxGeometry />
-          <meshNormalMaterial />
-        </mesh>
+      {/* Countertop left */}
+      <mesh scale={[1, 1, 4]} position={[4, 0, 1]}>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
 
-        {/* Countertop back */}
-        <mesh scale={[6, 1, 1]} position={[1.5, 0, 3.5]}>
-          <boxGeometry />
-          <meshNormalMaterial />
-        </mesh>
+      {/* Countertop back */}
+      <mesh scale={[6, 1, 1]} position={[1.5, 0, 3.5]}>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
 
-        {/* Cubbort */}
-        <mesh scale={[4, 1, .5]} position={[1.5, 1.5, 4]}>
-          <boxGeometry />
-          <meshNormalMaterial />
-        </mesh>
+      {/* Cubbort */}
+      <mesh scale={[4, 1, 0.5]} position={[1.5, 1.5, 4]}>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
 
-        <mesh scale={10} rotation-x={-Math.PI * 0.5} position={[0, -0.5, 0]}>
-          <planeGeometry scale={5} />
-          <meshBasicMaterial color={"lightBlue"} />
-        </mesh>
-      </>
-    );
-
+      <mesh scale={10} rotation-x={-Math.PI * 0.5} position={[0, -0.5, 0]}>
+        <planeGeometry scale={5} />
+        <meshBasicMaterial color={"lightBlue"} />
+      </mesh>
+    </>
+  );
 }
 
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -21,29 +21,38 @@ function App() {
 
   return (
     <>
-          <MainHeader />
-          <div className='renderBox'>
-           <Canvas camera={{position:[-1.5,1.5,-2.5]}} >
-            
+      <MainHeader />
+      <div className="renderBox">
+        <Canvas camera={{ position: [-1.5, 1.5, -2.5] }}>
+          {orbitControls && <OrbitControls />}
 
-           {orbitControls && (<OrbitControls />)}
-              
-
-            <ScrollControls pages={3} damping={0.1} horizontal={false}>
-              <Main />
-              <Scroll></Scroll>
-              <Scroll html>
-
-
-                
-              </Scroll>
-
-            </ScrollControls>
-
-           </Canvas>
-          </div>
+          <ScrollControls pages={4} damping={0.1} horizontal={false}>
+            <Main />
+            <Scroll></Scroll>
+            <Scroll html>
+              <div 
+                id="wasserhahn"  
+                style={{position: 'absolute', top: '0vh', color: 'black'}}
+                >
+                  Wasserhan
+                </div>
+              <div 
+                id="handtuchhalter" 
+                style={{position: 'absolute', top: "100vh", color: 'black'}}
+              >
+                  handtuchhalter
+              </div>
+              <div 
+                id="harkensystem"
+                style={{position: 'absolute', top: '200vh', color: 'black'}}
+              >
+                harkensystem
+              </div>
+            </Scroll>
+          </ScrollControls>
+        </Canvas>
+      </div>
     </>
-    
   );
 }
 
