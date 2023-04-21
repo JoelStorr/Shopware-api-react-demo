492571d831cb96fd2c0b92c60f506caa2ef6f6c7
Joel Storr <joel.storr@ewe.net>
2023-04-21 21:09:33 +0200

    Refactored API Categories Handling

--- src/components/Header/nav/Navigation.jsx
+++ src/components/Header/nav/Navigation.jsx
@@ -12,119 +12,26 @@ export default function Navigation() {
   const [isHovering, setIsHovering] = useState(false);
   const [endOfDemo, setEndOfDemo] = useState(false)
 
-  let data = null;
+
 
   useEffect(() => {
-    /*  if(!dataLoaded){
-          apiVal();
-          setDataLoaded(true)
-        } */
+      apiCategoriesHelper()
+        .then(
+          (data)=>{
+            setCategories(data)
+            setSortedSub(true);
+          }
+        );
 
-    //setCategories([]);
-    apiVal();
   }, []);
 
-
-
   useEffect(()=>{
-    if(fetchedData){
-      sortSubCategories();
+    if(categories.length > 0){
+      setSortedSub(true);
+      console.log('------------ Sorted Data --------------')
+      console.log(categories);
     }
-  },[fetchedData])
-
-
-  //NOTE: Change API Key
-  function apiVal (){
-    axios({
-      method: "post",
-      url: `${shopwareDomain}category`,
-      headers: { "sw-access-key": "SWSCWDHDQLQ4UM9YZZZIEUXLBQ" },
-    }).then((res) => {
-      //console.log(res.data.elements);
-      //setCategories([]);
-      orderNav(res.data.elements);
-    });
-  };
-
-  //NOTE: Only allows for one Child Categorie right now
-  //TODO: Make sure that there are no duplicates in Categories Array when data is called again.--- done
-
-  function orderNav(queryResElemetns) {
-    const tempCategories = [];
-
-    queryResElemetns.forEach((element) => {
-      //console.info(element);
-
-      if (
-        element.parentId === null &&
-        !tempCategories.some((ele) => element.name == ele.parent.name)
-      ) {
-        //console.log("Is base element");
-
-        const categoryConversionObject = { parent: element, children: [] };
-
-        tempCategories.push(categoryConversionObject);
-        //console.info(tempCategories);
-      } else if (element.parentId != null && tempCategories.length > 0) {
-        const parent = tempCategories.find((ele) => {
-          return ele.parent.id == element.parentId;
-        });
-
-        //console.log(parent);
-
-        parent.children.push(element);
-      }
-    });
-    setCategories(tempCategories);
-
-    // TODO: Reorder Children based on ID --- done
-    setFetchedData(true);
-  }
-
-  // TODO: Make sorting Class that can resort elements based on afterCategorieId
-  function sortSubCategories() {
-    const categoreisCopy = [...categories];
-
-    //console.log('Sort Sub Categories');
-
-    //console.log(categoreisCopy);
-
-    categoreisCopy.forEach((element) => {
-      
-      
-      
-      //console.log("entered foreach loop");
-  
-
-      //Match afterId to prev id
-      let sortedArray = element.children.sort((val1, val2)=>{
-          if(val1.afterCategoryId === null){
-            return -1;
-          }else if(val2.afterCategoryId === null){
-            return 1;
-          }else if(val1.afterCategoryId.normalize() === val2.id.normalize()){
-            return 1;
-          }else if(val2.afterCategoryId.normalize() === val1.id.normalize()){
-            return -1;
-          }
-          return 0;
-
-        })
-      
-
-      //Send it back into the State
-        
-        console.log(sortedArray);
-        element.children = sortedArray
-
-    });
-
-
-    setCategories(categoreisCopy);
-    console.log('Categoreis:', categories);
-    setSortedSub(true);
-
-  }
+  },[categories])
 
 
  function handleMouseOver(el){
@@ -168,12 +75,6 @@ export default function Navigation() {
 
       console.log('Tried Scrolling')
     }
-
-
-
-
-    let checkData = apiCategoriesHelper();
-    console.log(checkData)
 
 
   return (
