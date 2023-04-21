6a811372549ae3099bb78723baeecfac747ecf08
Joel Storr <joel.storr@ewe.net>
2023-04-21 11:05:01 +0200

    Page Detail CSS Design Added

--- src/components/Main/Overlays/ShopDetailOverlay.jsx
+++ src/components/Main/Overlays/ShopDetailOverlay.jsx
@@ -74,6 +74,22 @@ export default function ShopDetailOverlay() {
           </div>
           <div className="detailScreen-detail-box">
             <h1>Demo Data</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam
+              voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+              dolor sit amet.
+            </p>
+          <div className="detailScreen-btn-box">
+            <button>Remember Later</button>
+            <button>Add to Cart</button>
+          </div>
           </div>
         </div>
       )}
