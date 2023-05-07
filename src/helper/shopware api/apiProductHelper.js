a91a4473c54977fe8c7a2ed2a03e248c302e7e61
Joel Storr <joel.storr@ewe.net>
2023-05-07 18:54:42 +0200

    Fixed Naiming Error

--- src/helper/shopware api/apiProductHelper.js
+++ src/helper/shopware api/apiProductHelper.js
@@ -7,8 +7,7 @@ import StoreApiRequest from "./apiHelper";
 export async function getProductList(categoryID){
 
 
-    let listData = await StoreApiRequest.productList(categoryID).then((res) => {
-      console.log(res);
+    let listData = await StoreApiRequest.getProductList(categoryID).then((res) => {
       return res;
     });
     return Promise.resolve(listData);
